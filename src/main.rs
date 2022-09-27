77e2eacf35dfe3957d5739904c79011d44abebb2
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2022-09-28 02:01:14 +0300

    test initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Error;
+
+use rustfire_core::{
+    board::{Board, MinionSlot},
+    minions::generate_test_minion,
+};
+
+fn main() -> Result<(), Error> {
+    let mut board = Board::new();
+    let minion = generate_test_minion();
+    board.summon_minion(&minion, 1)?;
+
+    if let Some(minionslot) = board.player1.minionslots.get_mut(0) {
+        match minionslot {
+            MinionSlot::Minion(minion) => {
+                println!("Minion: {}", minion.name);
+            }
+            MinionSlot::None => {}
+        }
+    }
+    Ok(())
 }
